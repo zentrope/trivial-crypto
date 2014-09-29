66a78aa90886f2f9d0b94f598785f03630b27186
Keith Irwin <keith@zentrope.com>
2014-09-28 17:13:44 -0700

    A team is not a person with 12 legs.

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject com.zentrope/trivial-crypto "0.1.0"
+  :description "Simple crypto functions for strings."
+  :url "https://github.com/zentrope/simple-crypto"
+  :license {:name "EPL" :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
---
+++ src/crypto/core.clj
@@ -0,0 +1,91 @@
+(ns crypto.core
+  (:import
+    javax.crypto.spec.SecretKeySpec
+    javax.crypto.SecretKeyFactory
+    javax.crypto.Cipher
+    javax.crypto.spec.PBEKeySpec
+    javax.crypto.spec.DESKeySpec
+    javax.crypto.spec.IvParameterSpec))
+
+;;-----------------------------------------------------------------------------
+
+(def ^:private salt
+  (byte-array [1 2 3 4 5 6 7 8]))
+
+(def ^:private iterations
+  65536)
+
+(def ^:private key-len
+  256)
+
+(defn- iv-params
+  [encryptor]
+  (-> (.getParameters encryptor)
+      (.getParameterSpec IvParameterSpec)
+      (.getIV)
+      (IvParameterSpec.)))
+
+(defn- des-cipher
+  [passphrase]
+  (let [factory (SecretKeyFactory/getInstance "DES")
+        spec (DESKeySpec. (.getBytes passphrase))
+        tmp (.generateSecret factory spec)
+        secret (SecretKeySpec. (.getEncoded tmp) "DES")
+        encipher (doto (Cipher/getInstance "DES/ECB/PKCS5Padding")
+                    (.init Cipher/ENCRYPT_MODE secret))
+        decipher (doto (Cipher/getInstance "DES/ECB/PKCS5Padding")
+                   (.init Cipher/DECRYPT_MODE secret))]
+    [encipher decipher]))
+
+(defn- aes-cipher
+  [passphrase]
+  (let [factory (SecretKeyFactory/getInstance "PBKDF2WithHmacSHA1")
+        spec (PBEKeySpec. (.toCharArray passphrase) salt iterations key-len)
+        tmp (.generateSecret factory spec)
+        secret (SecretKeySpec. (.getEncoded tmp) "AES")
+        encipher (doto (Cipher/getInstance "AES/CBC/PKCS5Padding")
+                    (.init Cipher/ENCRYPT_MODE secret))
+        decipher (doto (Cipher/getInstance "AES/CBC/PKCS5Padding")
+                   (.init Cipher/DECRYPT_MODE secret (iv-params encipher)))]
+    [encipher decipher]))
+
+;;-----------------------------------------------------------------------------
+
+(defn- cipher
+  [type passphrase]
+  (case type
+    :aes (aes-cipher passphrase)
+    :des (des-cipher passphrase)
+    (throw (ex-info (format "Bad type: [%s], try [:aes :des]." type) {}))))
+
+(defn bytes->hex
+  [bytes]
+  (->> bytes (map (partial format "%02x")) (apply str)))
+
+(defn hex->bytes
+  [s]
+  (letfn [(->byte [c1 c2]
+            (unchecked-byte
+             (+ (bit-shift-left (Character/digit c1 16) 4)
+                (Character/digit c2 16))))]
+    (-> (mapv #(apply ->byte %) (partition 2 s))
+        (byte-array))))
+
+;;-----------------------------------------------------------------------------
+
+(defn encrypt
+  [{:keys [encryptor]} string]
+  (u/bytes->hex (.doFinal encryptor (.getBytes string "UTF-8"))))
+
+(defn decrypt
+  [{:keys [decryptor]} data]
+  (String. (.doFinal decryptor (u/hex->bytes data))))
+
+(defn make-crypto
+  [type passphrase]
+  (try
+    (let [[en de] (cipher type passphrase)]
+      {:type type :encryptor en :decryptor de})
+    (catch java.security.InvalidKeyException e
+      (throw (ex-info "Can't make key. Are the US JCE jars installed?"
+                      {:jvm (System/getProperty "java.home")} e)))))
